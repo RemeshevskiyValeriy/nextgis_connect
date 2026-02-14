c1938f046dfe3f9ce062d31e4d4ae6797a9e6fe6
Ivan Barsukov <i.barsukov@protonmail.com>
2026-02-14 14:12:21 +0300

    Add garbage collection in stop_qgis for better cleanup

--- tests/ng_connect_testcase.py
+++ tests/ng_connect_testcase.py
@@ -1,4 +1,5 @@
 import atexit
+import gc
 import json
 import os
 import sys
@@ -304,6 +305,10 @@ def stop_qgis() -> None:
 
     if APPLICATION_INFO is None:
         return
+
+    for _ in range(3):
+        gc.collect()
+        QgsApplication.processEvents()
 
     APPLICATION_INFO.application.exitQgis()
     del APPLICATION_INFO.application
