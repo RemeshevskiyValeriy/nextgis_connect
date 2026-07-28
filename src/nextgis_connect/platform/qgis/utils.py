d261147f40afb390c8b8f471bee28508da69f1b7
Ivan Barsukov <i.barsukov@protonmail.com>
2026-07-28 15:28:43 +0300

    Move chooser dialog to legacy

--- src/nextgis_connect/platform/qgis/utils.py
+++ src/nextgis_connect/platform/qgis/utils.py
@@ -16,17 +16,11 @@ from qgis.PyQt.QtCore import (
     QByteArray,
     QLocale,
     QMimeData,
-    Qt,
 )
 from qgis.PyQt.QtGui import QClipboard
 from qgis.PyQt.QtWidgets import (
     QAction,
-    QDialog,
-    QDialogButtonBox,
-    QListWidget,
-    QListWidgetItem,
     QMenu,
-    QVBoxLayout,
 )
 
 from nextgis_connect.legacy.settings.ng_connect_settings import (
@@ -53,57 +47,6 @@ def _iface() -> "QgisInterface":
     iface = qgis.utils.iface
     assert isinstance(iface, QgisInterface)
     return iface
-
-
-class ChooserDialog(QDialog):
-    """Show a simple multi-selection dialog.
-
-    Present a list of text options and store the selected values after
-    the dialog is accepted.
-
-    :ivar options: Text options shown to the caller.
-    :ivar seleced_options: Text options selected by the user.
-    """
-
-    def __init__(self, options):
-        """Initialize the chooser dialog.
-
-        :param options: Text options to display.
-        """
-        super().__init__()
-        self.options = options
-
-        self.setLayout(QVBoxLayout())
-
-        self.list = QListWidget()
-        self.list.setSelectionMode(QListWidget.SelectionMode.MultiSelection)
-        self.list.setSelectionBehavior(
-            QListWidget.SelectionBehavior.SelectItems
-        )
-        self.layout().addWidget(self.list)
-
-        for option in options:
-            item = QListWidgetItem(option)
-            self.list.addItem(item)
-
-        self.list.setCurrentRow(0)
-
-        self.btn_box = QDialogButtonBox(
-            QDialogButtonBox.StandardButton.Ok, Qt.Orientation.Horizontal, self
-        )
-        ok_button = self.btn_box.button(QDialogButtonBox.StandardButton.Ok)
-        assert ok_button is not None
-        ok_button.clicked.connect(self.accept)
-        self.layout().addWidget(self.btn_box)
-
-        self.seleced_options = []
-
-    def accept(self):
-        """Accept the dialog and store selected options."""
-        self.seleced_options = [
-            item.text() for item in self.list.selectedItems()
-        ]
-        super().accept()
 
 
 def open_plugin_help():
